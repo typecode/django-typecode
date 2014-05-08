1ea76d2be1053aa15cbc8c56834169b12bd93cb2
Lev Kanter <levkanter@gmail.com>
2014-05-08 14:14:00 -0400

    Update widgets.py

--- typecode/widgets.py
+++ typecode/widgets.py
@@ -1,48 +1,25 @@
 
 
-import os
 from django.contrib.admin.widgets import AdminFileWidget
-from django.utils.translation import ugettext as _
 from django.utils.safestring import mark_safe
 from django.conf import settings
-from PIL import Image
 
 
-'''
-adapted from snippet via https://djangosnippets.org/snippets/2455/
-'''
-try:
-    from easy_thumbnails.files import get_thumbnailer
+class AdminThumbImage(AdminFileWidget):
 
-    def thumbnail(image_path):
-        thumbnailer = get_thumbnailer(image_path)
-        thumbnail_options = {'crop': True, 'size': (160, 120), 'detail': True, 'upscale':True }
-        t = thumbnailer.get_thumbnail(thumbnail_options)
-        media_url = settings.MEDIA_URL
-        return u'<img src="%s%s" alt="%s" width="160" height="120"/>' % (media_url, t, image_path)
-except ImportError:
-    def thumbnail(image_path):
-        absolute_url = os.path.join(settings.MEDIA_ROOT, image_path)
-        return u'<img src="%s" alt="%s" />' % (absolute_url, image_path)
+    thumbnail_options = {
+        'crop': True,
+        'size': (200, 200),
+        'detail': True,
+        'upscale': True
+    }
 
-
-class AdminImage(AdminFileWidget):
-    '''
-    A FileField Widget that displays an image instead of a file path
-    if the current file is an image.
-    '''
     def render(self, name, value, attrs=None):
-        output = []
-        file_name = str(value)
-        if file_name:
-            file_path = '%s%s' % (settings.MEDIA_URL, file_name)
-            try:            # is image
-                Image.open(os.path.join(settings.MEDIA_ROOT, file_name))
-                output.append('<a target="_blank" href="%s">%s</a>' % \
-                    (file_path, thumbnail(file_name),))
-            except IOError: # not image
-                output.append('%s <a target="_blank" href="%s">%s</a> <br />%s ' % \
-                    (_('Currently:'), file_path, file_name, _('Change:')))
-
-        output.append(super(AdminFileWidget, self).render(name, value, attrs))
-        return mark_safe(u''.join(output))
+        out = []
+        v = str(value)
+        if v:
+            thumb = value.get_thumbnail(self.thumbnail_options)
+            thumb_path = settings.MEDIA_URL + str(thumb)
+            out.append(u'<img src="%s" alt="%s thumbnail">' % (thumb_path, v))
+        out.append(super(AdminThumbImage, self).render(name, value, attrs))
+        return mark_safe(u''.join(out))
