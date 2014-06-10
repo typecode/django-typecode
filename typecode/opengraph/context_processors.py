831831cfde96cee8b8d9d0165e780f3648ad5245
Lev Kanter <levkanter@gmail.com>
2014-06-10 11:37:21 -0400

    added open graph app

---
+++ typecode/opengraph/context_processors.py
@@ -0,0 +1,48 @@
+
+
+from django.conf import settings
+from django.contrib.sites.models import Site
+from django.contrib.staticfiles.storage import staticfiles_storage as ss
+
+from typecode.utils import relative_to_fq_url
+
+
+DEFAULT_TYPE = settings.get('OPENGRAPH_DEFAULT_TYPE', 'website')
+
+DEFAULT_LOCALE = settings.get('OPENGRAPH_DEFAULT_LOCALE', 'en_US')
+
+DEFAULT_TITLE = settings.get('OPENGRAPH_DEFAULT_TITLE', Site.objects.get_current().name)
+
+DEFAULT_DESCRIPTION = settings.get('OPENGRAPH_DEFAULT_DESCRIPTION', '')
+
+if hasattr(settings, 'OPENGRAPH_DEFAULT_IMAGE'):
+    DEFAULT_IMAGE = relative_to_fq_url(ss.url(settings.OPENGRAPH_DEFAULT_IMAGE))
+else:
+    DEFAULT_IMAGE = None
+
+
+def opengraph(request):
+    default = {
+        'type': DEFAULT_TYPE,
+        'locale': DEFAULT_LOCALE,
+        'site_name': DEFAULT_TITLE,
+        'title': DEFAULT_TITLE,
+        'description': DEFAULT_DESCRIPTION,
+        'url': relative_to_fq_url(''),
+        'images': []
+    }
+    if DEFAULT_IMAGE:
+        default['images'].append(DEFAULT_IMAGE)
+
+    obj = getattr(request, 'opengraph', None)
+    if obj and hasattr(obj, 'get_opengraph'):
+        og = obj.get_opengraph()
+        for k, v in og.items():
+            if not v:
+                del og[k]
+
+        default.update(og)
+
+    return {
+        'opengraph': default
+    }
